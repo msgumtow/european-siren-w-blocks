a07ad79a8757729b1310fae02a8b59f741da7382
msgumtow <71787320+msgumtow@users.noreply.github.com>
2020-10-25 10:32:48 -0400

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,18 @@
+/**
+ * In a "forever" loop
+ * 
+ * Place a "while" loop with the condition (light level greater than/equal to 200) replacing "true."
+ * 
+ * Inside the while loop,
+ * 
+ * Play note C
+ * 
+ * Plot an LED at 2,2
+ * 
+ * Play note F
+ * 
+ * Unplot the LED at 2,2
+ */
+basic.forever(function () {
+	
+})
